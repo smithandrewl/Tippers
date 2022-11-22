bf265d66bfdb69295a29a86823f438d738c88199
Andrew Smith <andrew.smith@wwwoodproducts.com>
2022-11-21 19:01:39 -0600

    Made minor changes, added an autogenerated file to .gitignore.

--- Tippy/App.fs
+++ Tippy/App.fs
@@ -6,25 +6,26 @@ open Fabulous.XamarinForms
 open type View
 
 module App =
-    type Model = { Count: int }
+    type Model = { Count: int; Label: string }
 
     type Msg =
         | Increment
         | Decrement
+        | TextChange of string
 
-    let init () = { Count = 0 }
+    let init () = { Count = 0; Label = "" }
 
     let update msg model =
         match msg with
         | Increment -> { model with Count = model.Count + 1 }
         | Decrement -> { model with Count = model.Count - 1 }
-
+        | TextChange(text) -> { model with Label = text}
     let view model =
         Application(
             ContentPage(
                 "Tippy",
                 VStack() {
-                    Label("Hello from Fabulous v2!")
+                    Label(model.Label)
                         .font(namedSize = NamedSize.Title)
                         .centerTextHorizontal()
 
@@ -34,6 +35,7 @@ module App =
 
                         Button("Increment", Increment)
                         Button("Decrement", Decrement)
+                        Entry(model.Label, fun (txt) -> TextChange(txt))
                      })
                         .centerVertical(expand = true)
                 }
