6c8d47e3a27f952e47e6610ab808a353a7f56e75
Andrew Smith <andrew.smith@wwwoodproducts.com>
2022-11-23 15:31:27 -0600

    Started working on the UI of the project.

--- Tippers/App.fs
+++ Tippers/App.fs
@@ -6,36 +6,62 @@ open Fabulous.XamarinForms
 open type View
 
 module App =
-    type Model = { Count: int; Label: string }
+    type ServiceRating = {
+      Responsive: int
+      Overall: int
+      Friendly: int
+    }
+    
+    type Model = {
+      Cost: float
+      Rating: ServiceRating
+      
+    }
 
     type Msg =
-        | Increment
-        | Decrement
-        | TextChange of string
+        | CostChange of float
+        | ResponsiveChange of int
+        | FriendlyChange of int
+        | OverallChange of int
 
-    let init () = { Count = 0; Label = "" }
+    let init () = {
+      Cost = 0.0
+      Rating = {
+          Responsive = 5
+          Overall = 5
+          Friendly = 5
+      }
+    }
 
     let update msg model =
         match msg with
-        | Increment -> { model with Count = model.Count + 1 }
-        | Decrement -> { model with Count = model.Count - 1 }
-        | TextChange(text) -> { model with Label = text}
+        | CostChange cost -> { model with Cost = cost }
+        | ResponsiveChange(value) -> {model with Rating = { model.Rating with Responsive = value}}
+        | FriendlyChange(value) -> {model with Rating = {model.Rating with Friendly = value}}
+        | OverallChange(value) -> {model with Rating = {model.Rating with Overall = value}}
     let view model =
         Application(
             ContentPage(
-                "Tippy",
+                "Tippers",
                 VStack() {
-                    Label(model.Label)
+                    Label("Tippers")
                         .font(namedSize = NamedSize.Title)
                         .centerTextHorizontal()
 
                     (VStack() {
-                        Label($"Count is {model.Count}")
-                            .centerTextHorizontal()
-
-                        Button("Increment", Increment)
-                        Button("Decrement", Decrement)
-                        Entry(model.Label, fun (txt) -> TextChange(txt))
+                
+                        Label("Total Cost").centerTextHorizontal()
+                        Entry(model.Cost.ToString(), fun (value) -> CostChange(value |> float))
+                        
+                        Label("Was the server responsive? (1-10)").centerTextHorizontal()
+                        
+                        Entry(model.Rating.Responsive.ToString(), fun (txt) -> ResponsiveChange(txt |> int))
+                        
+                        Label("Was the server friendly? (1-10)").centerTextHorizontal()
+                        Entry(model.Rating.Friendly.ToString(), fun (txt) -> FriendlyChange(txt |> int))
+                        
+                        Label("How was your overall experience? (1-10)").centerTextHorizontal()
+                        Entry(model.Rating.Overall.ToString(), fun (txt) -> OverallChange(txt |> int))
                      })
                         .centerVertical(expand = true)
                 }
