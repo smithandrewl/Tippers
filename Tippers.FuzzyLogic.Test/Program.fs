831e565eb6214afb6b4f79c6213153643a6efc2a
Andrew Smith <andrew.smith@wwwoodproducts.com>
2022-11-23 12:59:41 -0600

    Added a test program to generate a CSV file showing the fuzzy set values for every input ranging from 0 to 100.

---
+++ Tippers.FuzzyLogic.Test/Program.fs
@@ -0,0 +1,12 @@
+﻿module Tippers.FuzzyLogic.Program
+
+open Tippers.FuzzyLogic
+
+printfn "'Value', 'Low', 'Med', 'High'"
+
+ 
+for i in 0 .. 100 do
+    let fuzzy = FuzzyVariable.grade (float i) 0.0 100.0
+
+    printfn "%f, %f, %f, %f" (float i) fuzzy.Low  fuzzy.Med fuzzy.High
+
